628e472a7453b6231338c6a523542ecd0fec18b0
Hady Salhab <hadysalhab1@gmail.com>
2020-02-23 00:12:53 +1100

    Using The Console

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,1 +1,30 @@
-alert("hello world");
+//Log to console
+console.log("Hello World");
+console.log(123);
+console.log(true);
+var greeting = "hello";
+console.log(greeting);
+console.group([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+
+//error
+console.error("This is some error");
+
+//clear
+console.clear;
+
+//warning
+console.warn("This is a warning");
+
+//Timing
+console.time("Hello");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.timeEnd("Hello");
