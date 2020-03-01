2c70e047cb1af1fba74a91e23aa129528e229f47
Hady Salhab <hadysalhab1@gmail.com>
2020-03-02 09:52:18 +1100

    Creating Elements

---
+++ DOM Manipulation & Events/create.js
@@ -0,0 +1,29 @@
+//Create Element
+const li = document.createElement("li");
+
+//Add class
+li.className = "collection-item";
+
+//Add id
+li.id = "new-item";
+
+//Add Attribute
+li.setAttribute("title", "New Item");
+
+//Create text node and append
+//appendChild means we want to put something inside another thing
+li.appendChild(document.createTextNode("Hello World"));
+
+//Create a new link element
+const link = document.createElement("a");
+//add classes
+link.className = "delete-item secondary-content";
+//add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+//append link into li
+li.appendChild(link);
+
+//Append li as child to ul
+document.querySelector("ul.collection").appendChild(li);
+
+console.log(li);
