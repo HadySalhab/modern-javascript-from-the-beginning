0a7fc5e51bc91c275f885d377356bef226c7be23
Hady Salhab <hadysalhab1@gmail.com>
2020-03-03 10:27:31 +1100

    Add js sandbox practise

---
+++ js_sandbox/practise.js
@@ -0,0 +1,14 @@
+//Console.
+console.log("Hello");
+console.log(true);
+console.log(12);
+console.log([3, 4, 2, 1, 2]);
+console.log(2.4);
+console.log({ a: 2, b: 2 });
+
+//variable
+let val;
+val = "welcome";
+val = { name: "Hady", age: 25 };
+val = 12.5 * 4;
+console.log(val);
