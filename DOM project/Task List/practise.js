e4d0a1364f0f565e5a04352eff48ac786290ec56
Hady Salhab <hadysalhab1@gmail.com>
2020-03-05 12:36:05 +1100

    Practice

--- DOM project/Task List/practise.js
+++ DOM project/Task List/practise.js
@@ -1,4 +1,54 @@
-const text = "ad";
-const text2 = "harry";
+const taskForm = document.getElementById("task-form");
+const taskFormInput = document.getElementById("task");
+const taskList = document.querySelector(".collection");
+const filterFormInput = document.getElementById("filter");
 
-console.log(text2.includes(text));
+filterFormInput.addEventListener("keyup", filter);
+taskForm.addEventListener("submit", addTask);
+taskList.addEventListener("click", removeTask);
+
+//Add task to the list when user submit
+function addTask(e) {
+  e.preventDefault();
+  if (taskFormInput.value === "") {
+    alert("Please Insert Some Task Before submitting");
+    return;
+  } else {
+    const li = createTaskListItem(taskFormInput.value);
+    const link = createTaskListItemLink();
+    li.appendChild(link);
+    taskList.appendChild(li);
+    clearFormInputText();
+  }
+}
+
+//Function to create the List Item
+function createTaskListItem(text) {
+  const li = document.createElement("li");
+  li.className = "collection-item";
+  li.appendChild(document.createTextNode(text));
+  return li;
+}
+
+//Function to Create the Link
+function createTaskListItemLink() {
+  const link = document.createElement("a");
+  link.className = "delete-item secondary-content";
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  return link;
+}
+
+function clearFormInputText() {
+  taskFormInput.value = "";
+}
+
+//Remove task from the DOM when the user click the x icon
+function removeTask(e) {
+  if (e.target.className === "fa fa-remove") {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function filter(e) {
+  console.log(e.target.value);
+}
