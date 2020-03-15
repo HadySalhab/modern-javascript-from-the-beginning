c82457b49839158d461183494bab4d3105ca1cac
Hady Salhab <hadysalhab1@gmail.com>
2020-03-15 15:55:49 +1100

    Add Comment

--- Course Material/easy http/app.js
+++ Course Material/easy http/app.js
@@ -1,13 +1,15 @@
-const http = new easyHTTP;
+const http = new easyHTTP();
 
-// Get Posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log(posts);
-//   }
-// });
+//Get Posts
+//declaring the callback on function call, we can use function declaration/expression
+//this callback takes an err and posts.
+http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(posts);
+  }
+});
 
 // Get Single Post
 // http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
@@ -20,8 +22,8 @@ const http = new easyHTTP;
 
 // Create Data
 const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post'
+  title: "Custom Post",
+  body: "This is a custom post"
 };
 
 // Create Post
@@ -43,8 +45,11 @@ const data = {
 // });
 
 // Delete Post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
+http.delete("https://jsonplaceholder.typicode.com/posts/1", function(
+  err,
+  response
+) {
+  if (err) {
     console.log(err);
   } else {
     console.log(response);
