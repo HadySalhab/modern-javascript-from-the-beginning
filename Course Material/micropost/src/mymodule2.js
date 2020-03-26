46fdc65dbfce912e228e842b51629b7ffaf9e27e
Hady Salhab <hadysalhab1@gmail.com>
2020-03-26 15:12:39 +1100

    Micropost project

---
+++ Course Material/micropost/src/mymodule2.js
@@ -0,0 +1,11 @@
+// export const person = {
+//   name: "Brad",
+//   age: 30
+// };
+
+// export function sayHello() {
+//   return `Hello ${person.name}`;
+// }
+
+// const greeting = "Hello World";
+// export default greeting;
